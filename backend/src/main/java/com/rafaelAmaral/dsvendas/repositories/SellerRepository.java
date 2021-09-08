d36e8bf0509c4a5e3ae66f8d25379f4156783023
RafaelAdeOliveira <rafael.amaraldeoliveira@gmail.com>
2021-09-08 18:30:03 -0300

    Layers

---
+++ backend/src/main/java/com/rafaelAmaral/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.rafaelAmaral.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rafaelAmaral.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
---
+++ backend/src/main/java/com/rafaelAmaral/dsvendas/service/SellerService.java
@@ -0,0 +1,23 @@
+package com.rafaelAmaral.dsvendas.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rafaelAmaral.dsvendas.dto.SellerDTO;
+import com.rafaelAmaral.dsvendas.entities.Seller;
+import com.rafaelAmaral.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll() {
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+}
